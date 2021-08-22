f5900e1f551dacf823c80a931717518d567ef3e8
apometricsTK <Tobias.Kast@t-online.de>
2021-08-22 16:21:21 +0200

    Add django_crispy_forms, crispy_bootstrap
    Amend: django_countries to requirements.txt

--- nichetix/settings.py
+++ nichetix/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "crispy_forms",
+    "crispy_bootstrap5",
     # own apps
     "users",
 ]
@@ -94,6 +96,9 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'nichetix.wsgi.application'
 

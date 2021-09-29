0da626797258476177a4c37403a21eafd5757590
apometricsTK <Tobias.Kast@t-online.de>
2021-09-29 17:32:59 +0200

    Fix: Messages working properly again

---
+++ nichetix/static/js/messages-init.js
@@ -0,0 +1,13 @@
+/*
+Init and show Bootstrap toasts
+*/
+
+window.onload = (event) => {
+    let toastList = document.getElementsByClassName("toast");
+    if (toastList.length > 0) {
+        for (let toast of toastList) {
+            let toastInit = new bootstrap.Toast(toast)
+            toastInit.show()
+        }
+    }
+};
